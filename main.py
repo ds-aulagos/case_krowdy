e1491148cfd5a5d08c5ae90480fa6cc878f35765
Cesar A L Rojas <cesar.rojas-ext@dex.co>
2023-04-11 23:29:16 -0300

    feat: add output folder

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ path_json = "./files/2023-04-11T04-36-09-824ZUniversidades.json"
 locale = spark._jvm.java.util.Locale
 locale.setDefault(locale.forLanguageTag("es-ES"))
 
-sql_transformer_lower = SQLTransformer(statement="SELECT *, lower(value) lower FROM __THIS__")
+sql_transformer = SQLTransformer(statement="SELECT *, lower(universidade) lower FROM __THIS__")
 tokenizer = Tokenizer(inputCol="lower", outputCol="token")
 stop_words_remover = StopWordsRemover(inputCol="token", outputCol="stop")
 sql_transformer_concat = SQLTransformer(statement="SELECT *, concat_ws(' ', stop) concat FROM __THIS__")
@@ -49,7 +49,7 @@ min_hash_lsh = MinHashLSH(inputCol="vector", outputCol="lsh", numHashTables=3)
 #===================================================================
 
 def read_csv(path_file):
-    return(spark.read.option("header",True).csv(path_file))
+    return(spark.read.option("header",True).option("quote", "\"").csv(path_file))
 
 def read_json(path_file):
     return(spark.read.option("multiline","true").option("mode", "PERMISSIVE").format("json").load(path_file))
@@ -59,59 +59,56 @@ def read_json(path_file):
 def  process_df():
     df_csv = read_csv(path_csv)\
                 .withColumn("candidate_id", F.col("candidateId"))\
-                .withColumn("value", F.regexp_replace(F.col("value"), "\"", ""))\
-                .select("candidate_id","value")
+                .withColumn("universidade", F.regexp_replace(F.col("value"), "\"", ""))\
+                .select("candidate_id","universidade")
 
     df_json = read_json(path_json)\
                 .withColumn("cod_inei", F.col("código INEI"))\
-                .withColumn("nombre", F.col("Nombre "))\
+                .withColumn("universidade", F.col("Nombre "))\
                 .withColumn("siglas", F.col("Siglas "))\
-                .select("nombre","siglas","cod_inei")
-
-    df_csv.show(truncate = False)
-    df_json.show(truncate = False)
-
-    print(df_csv.count())
-    print(df_json.count())
+                .select("universidade","siglas","cod_inei")
 
     df_value_siglas_lower = df_json.alias("j")\
-                                .join(df_csv.alias("c"), [F.lower("j.siglas") == F.lower("c.value")], "inner")\
+                                .join(df_csv.alias("c"), [F.lower("j.siglas") == F.lower("c.universidade")], "inner")\
                                 .select("*")
 
-    df_value_siglas_lower.show(truncate = False)
-    print(df_value_siglas_lower.count())
-
-    df_csv_diff = df_csv.join(df_value_siglas_lower, ["value"], "left_anti").distinct()
+    df_csv_diff = df_csv.join(df_value_siglas_lower, ["universidade"], "left_anti").distinct()
     df_json_diff = df_json.join(df_value_siglas_lower, ["siglas"], "left_anti").distinct()
 
-    df_csv_diff.show(truncate = False)
-    df_json_diff.show(truncate = False)
+    pipeline = Pipeline(stages=[sql_transformer, tokenizer, stop_words_remover, sql_transformer_concat, regex_tokenizer, n_gram, hashing_tf, min_hash_lsh]).fit(df_json_diff)
 
-    print(df_csv_diff.count())
-    print(df_json_diff.count())
-
-    pipeline = Pipeline(stages=[sql_transformer_lower, tokenizer, stop_words_remover, sql_transformer_concat, regex_tokenizer, n_gram, hashing_tf, min_hash_lsh])
-
-    pipelineFit = pipeline.fit(df_csv_diff)
-    result_csv = pipelineFit.transform(df_csv_diff)
+    result_csv = pipeline.transform(df_csv_diff)
     result_csv = result_csv.filter(F.size(F.col("ngram")) > 0)
-    #print(f"Example transformation ({result_csv.count()} csv_diff):")
-    result_csv.select('candidate_id', 'value', 'concat', 'char', 'ngram', 'vector', 'lsh').show(1)
+    #result_csv.select('candidate_id', 'universidade', 'concat', 'char', 'ngram', 'vector', 'lsh').show()
 
+    result_json = pipeline.transform(df_json_diff)
+    result_json = result_json.filter(F.size(F.col("ngram")) > 0)    
+    #result_json.select('cod_inei', 'universidade', 'concat', 'char', 'ngram', 'vector', 'lsh').show()
 
-#===================================================================
+    result = pipeline.stages[-1].approxSimilarityJoin(result_csv, result_json, 0.5, "jaccardDist")
 
-def main():
-    process_df()
+    #result.select('datasetA.candidate_id', 'datasetA.universidade', 'datasetB.universidade', 'jaccardDist').sort(F.col('datasetA.candidate_id')).show(truncate = False)
+
+    w = Window.partitionBy('datasetA.candidate_id')
+
+    result = result\
+                .withColumn('minDist', F.min('jaccardDist').over(w))\
+                .where(F.col('jaccardDist') == F.col('minDist'))\
+                .drop('minDist')
+
+    df_output_csv = result.select('datasetA.candidate_id', F.col('datasetA.universidade').alias('value'), F.col('datasetB.universidade').alias('universidade homologada')).toPandas()
+
+    df_output_csv.to_csv("./output/universidades_homologadas.csv")
+
     
 
-
+#===================================================================
 
 if __name__ == '__main__':
     t1 = datetime.now()
     print('started at:', t1)
     
-    main()
+    process_df()
 
     t2 = datetime.now()
     dist = t2 - t1
